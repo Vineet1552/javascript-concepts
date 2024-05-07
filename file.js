b6fc89a3fecb104a0fbea715407d3f0281a409f9
Vineet1552 <99524765+Vineet1552@users.noreply.github.com>
2024-05-07 21:06:56 +0530

    let var const

--- file.js
+++ file.js
@@ -1,2 +1,15 @@
 console.log("Vineet");
 console.log("javascript");
+
+
+// constant and variables
+const accountId = 112211;
+let name = "Vineet";
+var email = "vermavineet688@gmail.com";
+
+var email = "utkarsh992@gmail.com";
+
+mobile = "9999999999";
+
+console.log("name");
+console.table([accountId, name, email, mobile]);
