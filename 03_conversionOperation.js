1777b18eb2b233f52f3b33fdb1a9fc3ef86504d9
Vineet1552 <99524765+Vineet1552@users.noreply.github.com>
2024-05-08 20:16:47 +0530

    3rd lecture

---
+++ 03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "123asd";
+
+// console.log(score);
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let convert = Number(score);
+// console.log(convert);
+// console.log(typeof(convert));
+
+
+
+// let score1 = Boolean;
+
+// console.log(score1);
+// console.log(typeof(score1));
+// console.log(typeof score1);
+
+// let convert1 = Number(score1);
+// console.log(convert1);
+// console.log(typeof(convert1));
+
+
+let str = 123;
+console.log(str);
+console.log(typeof(str));
+
+
+let stringNumber = String(str);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
